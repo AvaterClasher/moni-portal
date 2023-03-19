e4b42d5cbc427e6623d15c1441a18dd0d0bf3b05
AvaterClasher <116944847+AvaterClasher@users.noreply.github.com>
2023-03-19 23:58:02 +0530

    new file:   .gitignore 	new file:   package-lock.json 	new file:   package.json 	new file:   public/model/baked.jpg 	new file:   public/model/portal.glb 	new file:   src/Experience.jsx 	new file:   src/index.html 	new file:   src/index.jsx 	new file:   src/shaders/portal/fragment.glsl 	new file:   src/shaders/portal/vertex.glsl 	new file:   src/style.css 	new file:   vite.config.js

---
+++ src/Experience.jsx
@@ -0,0 +1,19 @@
+import { OrbitControls, useTexture } from '@react-three/drei'
+import { useGLTF } from '@react-three/drei'
+
+
+export default function Experience()
+{
+    const {nodes} =useGLTF('./model/portal.glb')
+    const bakedTexture = useTexture('./model/baked.jpg')
+    bakedTexture.flipY = false
+
+    return <>
+        <color args={['#201919']} attach="background"/>
+        <OrbitControls makeDefault />
+        <mesh geometry={nodes.baked.geometry}>
+            <meshBasicMaterial map={bakedTexture} />
+        </mesh>
+
+    </>
+}
